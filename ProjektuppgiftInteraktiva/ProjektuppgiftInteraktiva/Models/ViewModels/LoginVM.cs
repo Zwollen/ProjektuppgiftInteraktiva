98c0bb59c1e95345994d8d27ebcd6898664b9697
erva1600 <erva1600@COSQ260PC05>
2017-11-03 14:54:39 +0100

    login

---
+++ ProjektuppgiftInteraktiva/ProjektuppgiftInteraktiva/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjektuppgiftInteraktiva.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Du måste fylla i Användarnamn")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Lösenord")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
