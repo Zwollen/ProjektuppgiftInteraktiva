2106de5c99e70360824b4b54a4c24262ba549542
erva1600 <erva1600@COSQ260PC05>
2017-11-03 11:47:32 +0100

    start

---
+++ ProjektuppgiftInteraktiva/ProjektuppgiftInteraktiva/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjektuppgiftInteraktiva.MvcApplication" Language="C#" %>
---
+++ ProjektuppgiftInteraktiva/ProjektuppgiftInteraktiva/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektuppgiftInteraktiva.Startup))]
+namespace ProjektuppgiftInteraktiva
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
